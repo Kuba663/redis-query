3227951e179ec52fd0eb33fdc82b354a3673b024
Kuba663 <kuba63b@gmail.com>
2020-09-09 19:14:02 +0200

    Update AST.py

--- RedisQuery/AST.py
+++ RedisQuery/AST.py
@@ -1,6 +1,7 @@
 from rejson import Client, Path
 import json
 from rply.token import BaseBox
+from enum import Enum
 class String(BaseBox):
     def __init__(self,value):
         self.value = value
@@ -18,15 +19,36 @@ class Path(BaseBox):
         else:
             return Path.rootPath()
     pass
+class CreateType(Enum):
+    DOCUMENT = 1
+    TEMPLATE = 2
+    BASED = 3
+    pass
 class Create(BaseBox):
-    def __init__(self,client : Client,path : Path,name : String,value : String):
+    def __init__(self,client : Client,type : CreateType,path : Path,name : String,value : String):
         self.client = client
+        self.type = type
         self.path = path
         self.name = name
         self.value = value
         pass
     def eval(self):
-        self.client.jsonset(self.name.eval(),self.path.eval(),json.loads(self.value.eval()),nx=True)
+        if self.type == CreateType.DOCUMENT:
+            self.client.jsonset(self.name.eval(),self.path.eval(),json.loads(self.value.eval()),nx=True)
+        elif self.type == CreateType.TEMPLATE:
+            f = open("templates/" + self.name + ".json","w+")
+            f.write(self.value)
+            f.close()
+        elif self.type == CreateType.BASED:
+            temp = json.dumps(open("templates/" + self.name + ".json","r").read())
+            i = 0
+            values =  tuple(self.value.eval().split(';'))
+            for key in json.loads(temp):
+                temp[key] = values[i]
+                pass
+            self.client.jsonset(self.name.eval(),self.path.eval(),temp,nx=True)
+        else:
+            return AssertionError()
         pass
     pass
 class Select(BaseBox):
