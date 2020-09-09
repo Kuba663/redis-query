774bb10159a2d4dbd57929590cfd3075e77ccb00
Kuba663 <kuba63b@gmail.com>
2020-09-09 19:13:32 +0200

    Update Parser.py

--- RedisQuery/Parser.py
+++ RedisQuery/Parser.py
@@ -2,7 +2,7 @@ from rply import ParserGenerator
 import RedisQuery
 class Parser(object):
     """description of class"""
-    pg = ParserGenerator(["STRING","ID","SELECT","FROM","OPEN_CLAUSE","CLOSE_CLAUSE","CITE","INSERT","INTO","CREATE","DOCUMENT","AT"])
+    pg = ParserGenerator(["STRING","ID","SELECT","FROM","OPEN_CLAUSE","CLOSE_CLAUSE","CITE","INSERT","INTO","CREATE","DOCUMENT","AT","WHERE","IS","COMMA","TEMPLATE"])
     def __init__(self,client: Client):
         self.client = client
         pass
@@ -12,9 +12,19 @@ class Parser(object):
     @pg.production('expression: PATH OPEN_CLAUSE expression CLOSE_CLAUSE')
     def expression_path(p):
         return RedisQuery.AST.Path(p[2])
+    @pg.production('statement: CREATE TEMPLATE expression OPEN_CLAUSE expression CLOSE_CLAUSE')
     @pg.production('statement: CREATE DOCUMENT expression OPEN_CLAUSE expression CLOSE_CLAUSE AT expression')
+    @pg.production('statement: CREATE DOCUMENT expression FROM expression AT expression WHERE expression')
     def statement_create(p):
-        return RedisQuery.AST.Create(self.client,p[7],p[2],p[4])
+        if p[1].gettokentype() == 'DOCUMENT':
+            if p[3].gettokentype() == 'FROM':
+                return RedisQuery.AST.Create(self.client,RedisQuery.AST.CreateType.BASED,p[6],p[2],p[8])
+            else:
+                return RedisQuery.AST.Create(self.client,RedisQuery.AST.CreateType.DOCUMENT,p[7],p[2],p[4])
+        elif p[1].gettokentype() == 'TEMPLATE':
+            return RedisQuery.AST.Create(self.client,RedisQuery.AST.CreateType.TEMPLATE,p[7],p[2],p[4])
+        else:
+            return AssertionError()
     @pg.production('statement: SELECT expression FROM expression')
     def statement_select(p):
         return RedisQuery.AST.Select(self.client,p[3],p[1])
